50b17461dab1c38258df4a71b983dd1b8d20533e
Hugo Arturo Rodriguez ramos <Hugo Arturo Rodriguez ramos>
2017-06-15 14:16:22 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/cetis108/cetis108_4avpr_appfinal/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.edu.cetis108.cetis108_4avpr_appfinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
